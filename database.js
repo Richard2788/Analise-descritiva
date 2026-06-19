72f2ba785ca1c91c94d30c98953a377e24ad58a6
Richard Marx <bellusci.richard@escola.pr.gov.br>
2026-06-19 11:37:28 -0300

    DATABASE: Template feito

---
+++ database.js
@@ -0,0 +1,81 @@
+/*
+    export const db = {
+    cursos: [
+        {
+            id: 1,
+            nome: 'Curso',
+            modalidade: 'Modalidade',
+            salarioMedioAtual: [
+                {
+                    cargo: 'Cargo',
+                    salario: 0.00
+                }
+            ],
+            cotas: {
+                2025: [
+                    {
+                        tipo: 'Publica',
+                        vagas: 0,
+                        candidatos: 0,
+                        CandidatoPorVaga: 0.123,
+                        notaMinima: 1234
+                    },
+                    {
+                        tipo: 'negrosPublica',
+                        vagas: 0,
+                        candidatos: 0,
+                        CandidatoPorVaga: 0.123,
+                        notaMinima: 1234
+                    },
+                    {
+                        tipo: 'Universal',
+                        vagas: 0, candidatos: 0,
+                        CandidatoPorVaga: 0.123,
+                        notaMinima: 1234
+                    },
+                ]
+            }
+        }
+    ]
+}
+*/
+
+export const db = {
+    cursos: [
+        {
+            id: 1,
+            nome: 'Curso',
+            modalidade: 'Modalidade',
+            salarioMedioAtual: [
+                {
+                    cargo: 'Cargo',
+                    salario: 0.00
+                }
+            ],
+            cotas: {
+                2025: [
+                    {
+                        tipo: 'Publica',
+                        vagas: 0,
+                        candidatos: 0,
+                        CandidatoPorVaga: 0.123,
+                        notaMinima: 1234
+                    },
+                    {
+                        tipo: 'negrosPublica',
+                        vagas: 0,
+                        candidatos: 0,
+                        CandidatoPorVaga: 0.123,
+                        notaMinima: 1234
+                    },
+                    {
+                        tipo: 'Universal',
+                        vagas: 0, candidatos: 0,
+                        CandidatoPorVaga: 0.123,
+                        notaMinima: 1234
+                    },
+                ]
+            }
+        }
+    ]
+}
